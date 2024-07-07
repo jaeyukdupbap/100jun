1659ca0f35abd9af485a7022297a3c91bfda3c92
minsu <shvltld0@gmail.com>
2024-07-08 03:30:54 +0900

    백준 2문제 풀었음

---
+++ src/_1271.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class _1271 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        System.out.println(n / m);
+        System.out.println(n % m);
+    }
+}
--- src/_1463.java
+++ src/_1463.java
@@ -7,10 +7,10 @@ public class _1463 {
         dp[1] = 0;
         for (int i=2; i<=N; i++) {
             dp[i] = dp[i-1] + 1;
-            if (N % 3 == 0) {
+            if (i % 3 == 0) {
                 dp[i] = Math.min(dp[i], dp[i/3] + 1);
             }
-            if (N % 2 == 0) {
+            if (i % 2 == 0) {
                 dp[i] = Math.min(dp[i], dp[i/2] + 1);
             }
         }
